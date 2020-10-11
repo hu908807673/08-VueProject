030179f1397e99a2a16c5dc464ff80ad1e802664
hudonglong <908807673@qq.com>
2020-10-11 21:57:43 +0800

    vue.congig.js

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,14 @@
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false，
+devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://182.92.128.115',
+        changeOrigin: true,
+        // pathRewrite: {
+        //   '^/api': '',
+        // },
+      },
+    },
+  },
 }
